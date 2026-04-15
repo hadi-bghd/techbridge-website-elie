3b9b0ec34b106cf4d4d750a3b891a09872596d89
hadi-bghd <hady.baghdadi@gmail.com>
2026-04-16 00:50:12 +0300

    Fix route matching for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   useLocation,
 } from "react-router-dom";
-import { scroller } from "react-scroll";
 
 import Nav from "./Components/Nav";
 import "./App.css";
@@ -13,33 +12,22 @@ import TechBridgePage from "./Sections/TechBridgePage";
 const AppContent = () => {
   const location = useLocation();
 
-  // Function to handle scrolling with a custom duration
-  const scrollTo = (name) => {
-    scroller.scrollTo(name, {
-      duration: 800,
-      delay: 0,
-      smooth: "easeInOutQuad",
-    });
-  };
-
   return (
     <>
-      {/* Conditionally render Nav */}
       {location.pathname !== "/recipes" && <Nav />}
 
-      {/* Routes */}
       <Routes>
         <Route path="/" element={<TechBridgePage />} />
         <Route path="/techbridge" element={<TechBridgePage />} />
+        <Route path="*" element={<TechBridgePage />} />
       </Routes>
-
     </>
   );
 };
 
 const App = () => {
   return (
-    <Router basename="/">
+    <Router>
       <AppContent />
     </Router>
   );
